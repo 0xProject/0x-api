d60774454b71fcd4c400815df9bd3dc292d704fc
fabioberger <me@fabioberger.com>
2020-01-31 17:18:06 +0100

    Add in root handler for MetaTransactions endpoint

--- src/routers/meta_transaction_router.ts
+++ src/routers/meta_transaction_router.ts
@@ -7,6 +7,7 @@ import { MetaTransactionService } from '../services/meta_transaction_service';
 export const createMetaTransactionRouter = (metaTransactionService: MetaTransactionService): express.Router => {
     const router = express.Router();
     const handlers = new MetaTransactionHandlers(metaTransactionService);
+    router.get('/', asyncHandler(MetaTransactionHandlers.rootAsync.bind(MetaTransactionHandlers)));
     /**
      * GET Transaction endpoint returns an unsigned 0x Transaction that when sent to
      * `executeTransaction` will execute a specified swap.
