be02d68139b54d5d7c02e5f2c94d68fef86c2724
fabioberger <me@fabioberger.com>
2020-02-06 16:50:59 +0100

    Fix linter

--- src/services/meta_transaction_service.ts
+++ src/services/meta_transaction_service.ts
@@ -35,7 +35,7 @@ export class MetaTransactionService {
     private static _range(rangeCount: number): number[] {
         return [...Array(rangeCount).keys()];
     }
-    private static _calculateProtocolFee = (numOrders: number, gasPrice: BigNumber): BigNumber => {
+    private static readonly _calculateProtocolFee = (numOrders: number, gasPrice: BigNumber): BigNumber => {
         return new BigNumber(150000).times(gasPrice).times(numOrders);
     }
     constructor() {
