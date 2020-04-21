1d9ad66b0f6720442eab037a1144c4027f66eca4
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2020-04-21 00:56:03 -0400

    Remove Rfqt from gasSchedule

--- src/config.ts
+++ src/config.ts
@@ -159,7 +159,6 @@ const EXCLUDED_SOURCES = (() => {
 
 const gasSchedule: { [key in ERC20BridgeSource]: number } = {
     [ERC20BridgeSource.Native]: 1.5e5,
-    [ERC20BridgeSource.Rfqt]: 1.5e5,
     [ERC20BridgeSource.Uniswap]: 3e5,
     [ERC20BridgeSource.LiquidityProvider]: 3e5,
     [ERC20BridgeSource.Eth2Dai]: 5.5e5,
