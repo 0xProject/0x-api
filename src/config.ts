6b26a88a72f4b7d13bd28948f4257da0f4980fed
xianny <xianny@gmail.com>
2020-09-11 16:32:14 -0700

    fix excluded sources

--- src/config.ts
+++ src/config.ts
@@ -275,6 +275,7 @@ export const PROTOCOL_FEE_MULTIPLIER = new BigNumber(70000);
 const EXCLUDED_SOURCES = (() => {
     switch (CHAIN_ID) {
         case ChainId.Mainnet:
+            return [];
         case ChainId.Kovan:
             return [
                 ERC20BridgeSource.Balancer,
