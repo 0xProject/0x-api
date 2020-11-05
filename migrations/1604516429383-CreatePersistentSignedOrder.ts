233eb1ed54c84458b7c7e5387ba6aa454a43605c
xianny <xianny@gmail.com>
2020-11-05 10:58:55 -0800

    fix typeorm syntax errors

--- migrations/1604516429383-CreatePersistentSignedOrder.ts
+++ migrations/1604516429383-CreatePersistentSignedOrder.ts
@@ -1,19 +1,25 @@
 import { OrderEventEndState } from '@0x/mesh-rpc-client';
 import { MigrationInterface, QueryRunner, Table, TableIndex } from 'typeorm';
 
-const OrderEventEndStateStrings = Object.keys(OrderEventEndState).filter(x => isNaN(parseInt(x, 10)));
+const OrderEventEndStateStrings = Object.keys(OrderEventEndState)
+    .filter(x => isNaN(parseInt(x, 10)))
+    .map(s => s.toUpperCase());
 
 export class CreatePersistentSignedOrder1604516429383 implements MigrationInterface {
     public indices = ['maker_address', 'maker_asset_data', 'taker_asset_data', 'fee_recipient_address'].map(
-        colName => new TableIndex({ columnNames: [colName] }),
+        colName => new TableIndex({ name: `persistent_signed_orders_${colName}`, columnNames: [colName] }),
     );
-    public tableName = 'persistent_signed_orders';
 
     // tslint:disable-next-line
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `CREATE TYPE "persistent_signed_orders_state_enum" AS ENUM(\'${OrderEventEndStateStrings.join(
+                `\',\'`,
+            )}\');`,
+        );
         await queryRunner.createTable(
             new Table({
-                name: this.tableName,
+                name: 'persistent_signed_orders',
                 columns: [
                     { name: 'hash', type: 'varchar', isPrimary: true },
                     { name: 'sender_address', type: 'varchar' },
@@ -33,17 +39,22 @@ export class CreatePersistentSignedOrder1604516429383 implements MigrationInterf
                     { name: 'remaining_fillable_taker_asset_amount', type: 'varchar' },
                     { name: 'maker_fee_asset_data', type: 'varchar' },
                     { name: 'taker_fee_asset_data', type: 'varchar' },
-                    { name: 'state', type: 'enum', enum: OrderEventEndStateStrings, default: OrderEventEndState.Added },
+                    {
+                        name: 'state',
+                        type: 'enum',
+                        enum: OrderEventEndStateStrings,
+                        default: `'${OrderEventEndState.Added}'`,
+                    },
                 ],
             }),
             true,
         );
-        await queryRunner.createIndices(this.tableName, this.indices);
+        await queryRunner.createIndices('persistent_signed_orders', this.indices);
     }
 
     // tslint:disable-next-line
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropIndices(this.tableName, this.indices);
-        await queryRunner.dropTable(this.tableName);
+        await queryRunner.dropTable('persistent_signed_orders', true, true, true);
+        await queryRunner.query(`DROP TYPE "persistent_signed_orders_state_enum"`);
     }
 }
