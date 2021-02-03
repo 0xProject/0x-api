768d851ac8bf385234ea4af0f4aacce868ddcd49
xianny <xianny@gmail.com>
2021-02-02 16:41:49 -0800

    stub out methods

--- src/utils/asset_swapper_orderbook.ts
+++ src/utils/asset_swapper_orderbook.ts
@@ -1,8 +1,7 @@
-import { APIOrder, LimitOrderFields, Orderbook } from '@0x/asset-swapper';
-import { SignedOrder } from '@0x/asset-swapper/lib/src/utils/market_operation_utils/types';
+import { Orderbook, SignedNativeOrder } from '@0x/asset-swapper';
 
-import { DEFAULT_PAGE, DEFAULT_PER_PAGE } from '../constants';
 import { OrderBookService } from '../services/orderbook_service';
+// tslint:disable
 
 export class AssetSwapperOrderbook extends Orderbook {
     constructor(public readonly orderbookService: OrderBookService) {
@@ -10,33 +9,33 @@ export class AssetSwapperOrderbook extends Orderbook {
     }
 
     // TODO: change to token address instead of asset data
-    public async getOrdersAsync(
-        makerAssetData: string,
-        takerAssetData: string,
-        pruneFn?: (o: SignedOrder<LimitOrderFields>) => boolean,
-    ): Promise<SignedOrder<LimitOrderFields>[]> {
-        const apiOrders = await this.orderbookService.getOrdersAsync(DEFAULT_PAGE, DEFAULT_PER_PAGE, {
-            makerAssetData,
-            takerAssetData,
-        });
-        const orders = apiOrders.records.map(apiOrderToOrderbookOrder);
-        const result = pruneFn ? orders.filter(pruneFn) : orders;
-        return result;
+    public async getOrdersAsync(): // makerToken: string,
+    // takerToken: string,
+    // pruneFn?: (o: SignedNativeOrder) => boolean,
+    Promise<SignedNativeOrder[]> {
+        return [];
+        // const apiOrders = await this.orderbookService.getOrdersAsync(DEFAULT_PAGE, DEFAULT_PER_PAGE, {
+        //     makerAssetData,
+        //     takerAssetData,
+        // });
+        // const orders = apiOrders.records.map(apiOrderToOrderbookOrder);
+        // const result = pruneFn ? orders.filter(pruneFn) : orders;
+        // return result;
     }
-    public async getBatchOrdersAsync(
-        makerTokens: string[],
-        takerToken: string,
-        pruneFn?: (o: SignedOrder<LimitOrderFields>) => boolean,
-    ): Promise<SignedOrder<LimitOrderFields>[][]> {
-        const apiOrders = await this.orderbookService.getBatchOrdersAsync(DEFAULT_PAGE, DEFAULT_PER_PAGE, makerTokens, [
-            takerToken,
-        ]);
-        const orders = apiOrders.records.map(apiOrderToOrderbookOrder);
-        const pruned = pruneFn ? orders.filter(pruneFn) : orders;
-        const groupedByMakerToken: SignedOrder<LimitOrderFields>[][] = makerTokens.map(token =>
-            pruned.filter(o => o.order.makerToken === token),
-        );
-        return groupedByMakerToken;
+    public async getBatchOrdersAsync(): // makerTokens: string[],
+    // takerToken: string,
+    // pruneFn?: (o: SignedNativeOrder) => boolean,
+    Promise<SignedNativeOrder[][]> {
+        return [];
+        // const apiOrders = await this.orderbookService.getBatchOrdersAsync(DEFAULT_PAGE, DEFAULT_PER_PAGE, makerTokens, [
+        //     takerToken,
+        // ]);
+        // const orders = apiOrders.records.map(apiOrderToOrderbookOrder);
+        // const pruned = pruneFn ? orders.filter(pruneFn) : orders;
+        // const groupedByMakerToken: SignedNativeOrder[][] = makerTokens.map(token =>
+        //     pruned.filter(o => o.order.makerToken === token),
+        // );
+        // return groupedByMakerToken;
     }
     // tslint:disable-next-line:prefer-function-over-method
     public async destroyAsync(): Promise<void> {
@@ -44,7 +43,7 @@ export class AssetSwapperOrderbook extends Orderbook {
     }
 }
 
-// TODO
-function apiOrderToOrderbookOrder(apiOrder: APIOrder): SignedOrder<LimitOrderFields> {
-    return (apiOrder as any) as SignedOrder<LimitOrderFields>;
-}
+// // TODO
+// function apiOrderToOrderbookOrder(apiOrder: APIOrder): SignedNativeOrder {
+//     return (apiOrder as any) as SignedNativeOrder;
+// }
