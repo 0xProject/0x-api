23f95230b08cc754f6792dfc850ed4c0a63c90f1
Phil Liao <phil@0x.org>
2021-06-01 17:14:00 -0500

    update quote server, finish implementation

--- src/utils/quote_server_client.ts
+++ src/utils/quote_server_client.ts
@@ -1,8 +1,12 @@
-import { SubmitReceipt, SubmitRequest } from '@0x/quote-server';
+import { SchemaValidator } from '@0x/json-schemas';
+import { schemas, SubmitRequest } from '@0x/quote-server';
+import { Fee } from '@0x/quote-server/lib/src/types';
+import { BigNumber } from '@0x/utils';
 import { AxiosInstance } from 'axios';
 import { Summary } from 'prom-client';
 
 import { RFQT_REQUEST_MAX_RESPONSE_MS } from '../config';
+import { logger } from '../logger';
 
 const MARKET_MAKER_LAST_LOOK_LATENCY = new Summary({
     name: 'market_maker_last_look_latency',
@@ -11,29 +15,18 @@ const MARKET_MAKER_LAST_LOOK_LATENCY = new Summary({
 });
 
 export class QuoteServerClient {
-    private static _verifyResponse(response: any, payload: SubmitRequest): boolean {
-        const { data } = response;
-        if (data === undefined) {
-            return false;
-        }
+    private readonly _schemaValidator: SchemaValidator;
 
-        if (data.proceedWithFill !== undefined) {
-            // data as SubmitReceipt;
-            // validate the fee is expected
-            const { fee } = data;
-            if (
-                fee.token === payload.fee.token &&
-                fee.amount === payload.fee.amount.toString() &&
-                fee.type === payload.fee.type
-            ) {
-                return data.proceedWithFill;
-            }
-
-            return data.proceedWithFill;
-        }
-        return true;
+    private static _areFeesEqual(fee1: Fee, fee2: Fee): boolean {
+        return fee1.amount.eq(fee2.amount) && fee1.token === fee2.token && fee1.type === fee2.type;
     }
-    constructor(private readonly _axiosInstance: AxiosInstance) {}
+
+    constructor(private readonly _axiosInstance: AxiosInstance) {
+        this._schemaValidator = new SchemaValidator();
+        this._schemaValidator.addSchema(schemas.feeSchema);
+        this._schemaValidator.addSchema(schemas.submitRequestSchema);
+        this._schemaValidator.addSchema(schemas.submitReceiptSchema);
+    }
 
     public async confirmLastLookAsync(makerUri: string, payload: SubmitRequest): Promise<boolean> {
         const timerStopFn = MARKET_MAKER_LAST_LOOK_LATENCY.labels(makerUri).startTimer();
@@ -42,11 +35,23 @@ export class QuoteServerClient {
                 params: payload,
                 timeout: RFQT_REQUEST_MAX_RESPONSE_MS,
             });
-            return QuoteServerClient._verifyResponse(response, payload);
-        } catch (err) {
+            this._schemaValidator.validate(response.data, schemas.submitReceiptSchema);
+            const responseFee: Fee = {
+                amount: new BigNumber(response.data.fee.amount),
+                token: response.data.fee.token,
+                type: response.data.fee.type,
+            };
+
+            if (!QuoteServerClient._areFeesEqual(responseFee, payload.fee)) {
+                throw new Error('Fee in response is not equal to fee in request');
+            }
+
+            return response.data.proceedWithFill;
+        } catch (error) {
+            logger.warn({ error, makerUri }, 'Encountered an error when confirming last look with market maker');
+            return false;
         } finally {
             timerStopFn();
         }
-        return true;
     }
 }
