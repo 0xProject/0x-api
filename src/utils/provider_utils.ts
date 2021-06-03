eebe021b2ed4864b5d5da46fe84dcadd643af89f
Alex Kroeger <alexrkroeger@gmail.com>
2021-06-03 15:22:14 -0700

    get rid of random empty line

--- src/utils/provider_utils.ts
+++ src/utils/provider_utils.ts
@@ -7,7 +7,6 @@ export const providerUtils = {
     createWeb3Provider: (rpcHost: string): SupportedProvider => {
         const providerEngine = new Web3ProviderEngine();
         providerEngine.addProvider(new RPCSubprovider(rpcHost));
-
         ZeroExProviderUtils.startProviderEngine(providerEngine);
         return providerEngine;
     },
