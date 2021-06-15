5def887bcb3774d7ffc98e6741c041723808f281
Phil Liao <phil@0x.org>
2021-06-15 17:42:31 -0500

    fix build

--- test/services/rfqm_service_test.ts
+++ test/services/rfqm_service_test.ts
@@ -1226,13 +1226,13 @@ describe('RfqmService', () => {
                 createdAt: new Date(transaction1Time),
                 orderHash: '0x00',
                 transactionHash: '0x01',
-                status: RfqmTranasctionSubmissionStatus.Reverted,
+                status: RfqmTranasctionSubmissionStatus.RevertedConfirmed,
             });
             const submission2 = new RfqmTransactionSubmissionEntity({
                 createdAt: new Date(transaction2Time),
                 orderHash: '0x00',
                 transactionHash: '0x02',
-                status: RfqmTranasctionSubmissionStatus.Successful,
+                status: RfqmTranasctionSubmissionStatus.SucceededConfirmed,
             });
 
             const dbUtilsMock = mock(RfqmDbUtils);
