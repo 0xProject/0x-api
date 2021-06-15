f6caef1649e6a57486ab2879161e30d249678912
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2021-06-15 13:48:52 -0600

    Change migration to use typeorm apis

--- migrations/1623716682037-DropRfqmStatusReasonColumn.ts
+++ migrations/1623716682037-DropRfqmStatusReasonColumn.ts
@@ -1,13 +1,20 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export class DropRfqmStatusReasonColumn1623716682037 implements MigrationInterface {
     name = 'DropRfqmStatusReasonColumn1623716682037';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "rfqm_jobs" DROP COLUMN "status_reason"`);
+        await queryRunner.dropColumn('rfqm_jobs', 'status_reason');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "rfqm_jobs" ADD "status_reason" character varying`);
+        await queryRunner.addColumn(
+            'rfqm_jobs',
+            new TableColumn({
+                isNullable: true,
+                name: 'status_reason',
+                type: 'varchar'
+            }),
+        );
     }
 }
