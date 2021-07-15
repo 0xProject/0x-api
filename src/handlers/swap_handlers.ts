820c76788c04442c1124bee61d6c0d67820e155c
Lawrence Forman <me@merklejerk.com>
2021-07-15 17:15:09 -0400

    update deps

--- src/handlers/swap_handlers.ts
+++ src/handlers/swap_handlers.ts
@@ -43,6 +43,7 @@ import {
     ValidationErrorCodes,
     ValidationErrorReasons,
 } from '../errors';
+import { logger } from '../logger';
 import { schemas } from '../schemas';
 import { SwapService } from '../services/swap_service';
 import { GetSwapPriceResponse, GetSwapQuoteParams, GetSwapQuoteResponse } from '../types';
@@ -276,6 +277,7 @@ export class SwapHandlers {
             }
             return swapQuote;
         } catch (e) {
+            logger.error(`_getSwapQuoteAsync() failed: ${e.message || e}`);
             // If this is already a transformed error then just re-throw
             if (isAPIError(e)) {
                 throw e;
@@ -284,7 +286,7 @@ export class SwapHandlers {
             if (isRevertError(e)) {
                 throw new RevertAPIError(e);
             }
-            const errorMessage: string = e.message;
+            const errorMessage: string = typeof e === 'string' ? e : e.message;
             // TODO AssetSwapper can throw raw Errors or InsufficientAssetLiquidityError
             if (
                 errorMessage.startsWith(SwapQuoterError.InsufficientAssetLiquidity) ||
@@ -307,7 +309,7 @@ export class SwapHandlers {
                     },
                 ]);
             }
-            req.log.info('Uncaught error', e.message, e.stack);
+            req.log.error('Uncaught error', e.message, e.stack);
             throw new InternalServerError(e.message);
         }
     }
