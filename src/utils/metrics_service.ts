d2b5319743356a80b9123123c32e41049914085f
Daniel Pyrathon <daniel@0x.org>
2021-09-17 14:44:11 -0400

    refactor

--- src/utils/metrics_service.ts
+++ src/utils/metrics_service.ts
@@ -28,47 +28,30 @@ const ORDER_FILL_RATIO_WARNING_RANGE = new Counter({
 const RFQ_MAKER_NETWORK_INTERACTION_COUNTER = new Counter({
     name: 'rfq_maker_network_interaction_counter',
     help: 'Provides stats around market maker network interactions',
-    labelNames: [
-        'isLastLook',
-        'integratorLabel',
-        'url',
-        'quoteType',
-        'included',
-        'statusCode',
-        'market',
-    ],
+    labelNames: ['isLastLook', 'integratorLabel', 'url', 'quoteType', 'included', 'statusCode', 'market'],
 });
 
 const RFQ_MAKER_NETWORK_INTERACTION_SUMMARY = new Summary({
     name: 'rfq_maker_network_interaction_summary',
     help: 'Provides stats around market maker network interactions',
-    labelNames: [
-        'isLastLook',
-        'integratorLabel',
-        'url',
-        'quoteType',
-        'included',
-        'statusCode',
-        'market',
-    ],
+    labelNames: ['isLastLook', 'integratorLabel', 'url', 'quoteType', 'included', 'statusCode', 'market'],
 });
-
 
 // NOTE: Do not use this map for anything sensitive. This is only used for
 // metrics/datavis purposes.
-const KNOWN_TOKENS: {[key: string]: string} = {
-    "0x6b175474e89094c44da98b954eedeac495271d0f": "DAI",
-    "0xdac17f958d2ee523a2206206994597c13d831ec7": "USDT",
-    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48": "USDC",
-    "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599": "WBTC",
-    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": "WETH",
+const KNOWN_TOKENS: { [key: string]: string } = {
+    '0x6b175474e89094c44da98b954eedeac495271d0f': 'DAI',
+    '0xdac17f958d2ee523a2206206994597c13d831ec7': 'USDT',
+    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': 'USDC',
+    '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599': 'WBTC',
+    '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': 'WETH',
 };
 
 /**
  * Returns a human-readible label for Prometheus counters. Only popular most relevant pairs
  * should be displayed in Prometheus (since it overload the service) and any other market that does
  * not contain popular pairs will simply return "Other".
- * 
+ *
  * @param tokenSold the token being sold
  * @param tokenPurchased the token being purchased
  * @returns a market like "WETH-DAI", or "Other" is a pair is unknown
@@ -80,7 +63,7 @@ function getMarketLabel(tokenSold: string, tokenPurchased: string): string {
     const tokenA: string | undefined = KNOWN_TOKENS[items[0]];
     const tokenB: string | undefined = KNOWN_TOKENS[items[1]];
     if (!tokenA || !tokenB) {
-        return "Other";
+        return 'Other';
     }
     return `${tokenA}-${tokenB}`;
 }
