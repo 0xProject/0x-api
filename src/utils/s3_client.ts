63934ac06fa0d024839db03b307900ff3655f722
Xinxing Liu <xinxing@0xproject.com>
2022-02-15 17:39:11 -0800

    Clean up and add more comments

--- src/utils/s3_client.ts
+++ src/utils/s3_client.ts
@@ -1,18 +1,22 @@
 import { S3 } from 'aws-sdk';
 
 /**
- * S3Client wraps S3, making it far easier to unit test S3 and ignore S3 details
+ * S3Client wraps S3, abstracting away S3 details
  */
 export class S3Client {
     constructor(private readonly _s3: S3) {}
 
+    /**
+     * Check if a file exists in S3 without obtaining its content. If it exists, 
+     * returns the `lastModified` timestamp with the result.
+     */
     public async hasFileAsync(
         bucket: string,
-        key: string,
+        fileName: string,
     ): Promise<{ exists: boolean; lastModified: Date | undefined }> {
         var bucketParams = {
             Bucket: bucket,
-            Key: key,
+            Key: fileName,
         };
 
         try {
@@ -20,7 +24,7 @@ export class S3Client {
             if (response.LastModified) {
                 return { exists: true, lastModified: response.LastModified };
             } else {
-                throw new Error(`Failed to get metadata of S3 object ${key} in bucket ${bucket}.`);
+                throw new Error(`Failed to get metadata of S3 object ${fileName} in bucket ${bucket}.`);
             }
         } catch (error) {
             if (error.code === 'NotFound') {
@@ -31,17 +35,22 @@ export class S3Client {
         }
     }
 
-    public async getFileContentAsync(bucket: string, key: string): Promise<{ content: string; lastModified: Date }> {
+    /**
+     * Obtain the content of a file in S3. This method assumes the specified file exists
+     * and will throw if the request failed for whatever reasons. On successful query it
+     * always returns the `lastModified` timestamp with the result.
+     */
+    public async getFileContentAsync(bucket: string, fileName: string): Promise<{ content: string; lastModified: Date }> {
         var bucketParams = {
             Bucket: bucket,
-            Key: key,
+            Key: fileName,
         };
 
         const response = await this._s3.getObject(bucketParams).promise();
         if (response && response.Body && response.LastModified) {
             return { content: response.Body.toString(), lastModified: response.LastModified };
         } else {
-            throw new Error(`Failed to get content of S3 object ${key} in bucket ${bucket}.`);
+            throw new Error(`Failed to get content of S3 object ${fileName} in bucket ${bucket}.`);
         }
     }
 }
