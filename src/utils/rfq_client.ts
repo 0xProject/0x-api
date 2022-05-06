cc3d0ad2d0b2a21b3bd4bfffe997f1a46c168798
Phil Liao <phil@0x.org>
2022-05-06 17:57:56 -0500

    feat: use rfqClient to fetch quotes
    update asset swapper
    
    rollout to hash utils

---
+++ src/utils/rfq_client.ts
@@ -0,0 +1,107 @@
+import {
+    IRfqClient,
+    RfqClientV1PriceRequest,
+    RfqClientV1PriceResponse,
+    RfqClientV1QuoteRequest,
+    RfqClientV1QuoteResponse,
+} from '@0x/asset-swapper';
+import { AxiosInstance } from 'axios';
+import { OK } from 'http-status-codes';
+
+import { RFQT_REQUEST_MAX_RESPONSE_MS } from '../config';
+import { logger } from '../logger';
+
+export class RfqClient implements IRfqClient {
+    constructor(private readonly _rfqApiUrl: string, private readonly _axiosInstance: AxiosInstance) {}
+
+    /**
+     * Communicates to an RFQ Client to fetch available prices
+     */
+    public async getV1PricesAsync(request: RfqClientV1PriceRequest): Promise<RfqClientV1PriceResponse> {
+        try {
+            const response = await this._axiosInstance.post(
+                `${this._rfqApiUrl}/rfqt/v1/prices`,
+                {
+                    altRfqAssetOfferings: request.altRfqAssetOfferings,
+                    assetFillAmount: request.assetFillAmount,
+                    chainId: request.chainId,
+                    comparisonPrice: request.comparisonPrice,
+                    integratorId: request.integratorId,
+                    intentOnFilling: request.intentOnFilling,
+                    makerToken: request.makerToken,
+                    marketOperation: request.marketOperation,
+                    takerAddress: request.takerAddress,
+                    takerToken: request.takerToken,
+                    txOrigin: request.txOrigin,
+                },
+                {
+                    // tslint:disable-next-line: custom-no-magic-numbers
+                    timeout: RFQT_REQUEST_MAX_RESPONSE_MS + 150,
+                    headers: {
+                        '0x-chain-id': request.chainId,
+                    },
+                },
+            );
+
+            if (response.status !== OK) {
+                logger.warn({ request }, 'Unable to get RFQt v1 prices');
+                return {
+                    prices: [],
+                };
+            }
+
+            const priceResponse = response.data as RfqClientV1PriceResponse;
+            return priceResponse;
+        } catch (error) {
+            logger.error({ errorMessage: error.message }, 'Encountered an error fetching for /rfqt/v1/prices');
+            return {
+                prices: [],
+            };
+        }
+    }
+
+    /**
+     * Communicates to an RFQ Client to fetch available signed quotes
+     */
+    public async getV1QuotesAsync(request: RfqClientV1QuoteRequest): Promise<RfqClientV1QuoteResponse> {
+        try {
+            const response = await this._axiosInstance.post(
+                `${this._rfqApiUrl}/rfqt/v1/quotes`,
+                {
+                    altRfqAssetOfferings: request.altRfqAssetOfferings,
+                    assetFillAmount: request.assetFillAmount,
+                    chainId: request.chainId,
+                    comparisonPrice: request.comparisonPrice,
+                    integratorId: request.integratorId,
+                    intentOnFilling: request.intentOnFilling,
+                    makerToken: request.makerToken,
+                    marketOperation: request.marketOperation,
+                    takerAddress: request.takerAddress,
+                    takerToken: request.takerToken,
+                    txOrigin: request.txOrigin,
+                },
+                {
+                    // tslint:disable-next-line: custom-no-magic-numbers
+                    timeout: RFQT_REQUEST_MAX_RESPONSE_MS + 150,
+                    headers: {
+                        '0x-chain-id': request.chainId,
+                    },
+                },
+            );
+
+            if (response.status !== OK) {
+                logger.warn({ request }, 'Unable to get RFQt v1 quotes');
+                return {
+                    quotes: [],
+                };
+            }
+
+            return response.data as RfqClientV1QuoteResponse;
+        } catch (error) {
+            logger.error({ errorMessage: error.message }, 'Encountered an error fetching for /rfqt/v1/quotes');
+            return {
+                quotes: [],
+            };
+        }
+    }
+}
