99d0ba58fbb72200ee4352c7fd7e450cb3d0b275
Ido Kleinman <idokleinman@gmail.com>
2022-08-25 14:38:16 -0700

    Excluded sources update

--- src/config.ts
+++ src/config.ts
@@ -488,6 +488,7 @@ const EXCLUDED_SOURCES = (() => {
         case ChainId.Optimism:
         case ChainId.Goerli:
         case ChainId.PolygonMumbai:
+        case ChainId.ArbitrumRinkeby:
         case ChainId.Arbitrum:
             return [ERC20BridgeSource.MultiBridge, ERC20BridgeSource.Native];
         default:
