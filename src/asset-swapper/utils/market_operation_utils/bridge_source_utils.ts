b2c85f535f7e6e7c7c5aaa07deb176836c3f1505
Kyu <kyuhyun217@gmail.com>
2022-08-29 21:00:57 +0900

    Revert `isValidAddress` types

--- src/asset-swapper/utils/market_operation_utils/bridge_source_utils.ts
+++ src/asset-swapper/utils/market_operation_utils/bridge_source_utils.ts
@@ -61,8 +61,8 @@ import {
 } from './constants';
 import { CurveInfo, ERC20BridgeSource, PlatypusInfo } from './types';
 
-// tslint:disable-next-line: completed-docs ban-types
-export function isValidAddress(address: string | string): address is string {
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function isValidAddress(address: string | String): address is string {
     return (typeof address === 'string' || address instanceof String) && address.toString() !== NULL_ADDRESS;
 }
 
