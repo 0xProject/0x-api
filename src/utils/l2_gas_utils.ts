a5b5424bf83652004244c16797e8716a15a4ca8f
Kyu <kyuhyun217@gmail.com>
2022-09-20 09:13:05 -0700

    Add L1 gas cost for Arbitrum in `SwapHandlers`

--- src/utils/l2_gas_utils.ts
+++ src/utils/l2_gas_utils.ts
@@ -4,7 +4,9 @@ const ARBITRUM_FIXED_COST_ESTIMATE = 140;
 const TX_DATA_NON_ZERO_GAS_EIP2028 = 16;
 const L1_GAS_COST_BUFFER = 1.15; // 15% buffer
 
-const DEFAULT_ARBITRUM_L1_CALLDATA_PRICE_PER_UNIT = 32_000_000_000;
+// This number is based on "safe" (relatively large) value from `ArbGasInfo`'s (precompile) `getPricesInWei`
+// `getPricesInWei` returns price per byte which needs to be divided by 16 (calldata unit for non-zero byte).
+const DEFAULT_ARBITRUM_L1_CALLDATA_PRICE_PER_UNIT = 6_250_000_000; // 100b / 16
 
 // github.com/OffchainLabs/nitro/blob/c2f7c43dcd9a35a716cf6db8625228b4b7a4328d/arbos/l1pricing/l1pricing.go#L714
 function getByteCountAfterBrotli0(calldata: string): number {
