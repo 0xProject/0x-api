0676e7db79b22c4e8f5690319508843d163ad70d
Ido Kleinman <idokleinman@gmail.com>
2022-09-27 16:39:25 -0700

    lint

--- src/asset-swapper/utils/market_operation_utils/pools_cache/sgPoolDataService.ts
+++ src/asset-swapper/utils/market_operation_utils/pools_cache/sgPoolDataService.ts
@@ -74,7 +74,7 @@ const queryWithOutLinear = gql`
 const QUERY_BY_CHAIN_ID: { [chainId: number]: string } = {
     [ChainId.Mainnet]: queryWithLinear,
     [ChainId.Polygon]: queryWithOutLinear,
-    [ChainId.Arbitrum]: queryWithLinear
+    [ChainId.Arbitrum]: queryWithLinear,
 };
 
 const DEFAULT_MAX_POOLS_FETCHED = 96;
