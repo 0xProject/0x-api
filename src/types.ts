d3c6d962da00719658f915c5910a906a5e0b6a28
joshrrose <josh.r.rose@gmail.com>
2022-10-07 10:40:52 -0600

    ran yarn fix

--- src/types.ts
+++ src/types.ts
@@ -222,7 +222,7 @@ export interface SourceComparison {
 export type AffiliateFee =
     | { feeType: AffiliateFeeType.GaslessFee | AffiliateFeeType.PositiveSlippageFee; recipient: string }
     | {
-          feeType: AffiliateFeeType.None | AffiliateFeeType.PercentageFee; 
+          feeType: AffiliateFeeType.None | AffiliateFeeType.PercentageFee;
           recipient: string;
           sellTokenPercentageFee: number;
           buyTokenPercentageFee: number;
