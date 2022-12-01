58c63e4a00d4e58830871006be0a9ae05d7dd7a2
eobbad <35933488+eobbad@users.noreply.github.com>
2022-12-01 10:36:21 -0500

    enable compound on mainnet

--- src/asset-swapper/utils/market_operation_utils/constants.ts
+++ src/asset-swapper/utils/market_operation_utils/constants.ts
@@ -100,7 +100,7 @@ export const SELL_SOURCE_FILTER_BY_CHAIN_ID = valueByChainId<SourceFilters>(
             ERC20BridgeSource.Synthetix,
             ERC20BridgeSource.AaveV2,
             // TODO: Explore adding compound as a source
-            // ERC20BridgeSource.Compound,
+            ERC20BridgeSource.Compound,
         ]),
         [ChainId.Kovan]: new SourceFilters([ERC20BridgeSource.Native]),
         [ChainId.Goerli]: new SourceFilters([
@@ -250,7 +250,7 @@ export const BUY_SOURCE_FILTER_BY_CHAIN_ID = valueByChainId<SourceFilters>(
             ERC20BridgeSource.Synthetix,
             ERC20BridgeSource.AaveV2,
             // TODO: Explore adding compound as a source
-            // ERC20BridgeSource.Compound,
+            ERC20BridgeSource.Compound,
         ]),
         [ChainId.Goerli]: new SourceFilters([
             ERC20BridgeSource.Native,
@@ -2385,7 +2385,7 @@ export const AAVE_V2_SUBGRAPH_URL_BY_CHAIN_ID = valueByChainId(
 export const COMPOUND_API_URL_BY_CHAIN_ID = valueByChainId(
     {
         // TODO: enable after FQT has been redeployed on Ethereum mainnet
-        // [ChainId.Mainnet]: 'https://api.compound.finance/api/v2',
+        [ChainId.Mainnet]: 'https://api.compound.finance/api/v2',
     },
     null,
 );
