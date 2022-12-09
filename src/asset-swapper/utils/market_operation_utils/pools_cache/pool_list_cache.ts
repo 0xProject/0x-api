b95eb24f684bb1988df144e53592e4da2bb99731
Kerry <bandwwith@gmail.com>
2022-12-09 11:47:17 -0500

    fix: Updated graphql query.

--- src/asset-swapper/utils/market_operation_utils/pools_cache/pool_list_cache.ts
+++ src/asset-swapper/utils/market_operation_utils/pools_cache/pool_list_cache.ts
@@ -1,78 +1,87 @@
 import { request } from 'graphql-request';
 import gql from 'graphql-tag';
 
-const queryPools = (skip: number, pageSize: number, createdBy = '', dataProvider = '') => gql`
-    {
-      pools(first: ${pageSize}, skip: ${skip},
-        orderDirection: desc,
-        orderBy: createdAt,
-        where: {
-          createdBy_contains: "${createdBy}"
-          statusFinalReferenceValue_not: "Confirmed"
-          dataProvider_contains: "${dataProvider}"
-        }) {
+export const queryPools = (
+	skip: number,
+	pageSize: number,
+	createdBy = '',
+	dataProvider = ''
+) => gql`
+  {
+    pools(first: ${pageSize}, skip: ${skip},
+      orderDirection: desc,
+      orderBy: createdAt,
+      where: { 
+        createdBy_contains: "${createdBy}"
+        statusFinalReferenceValue_not: "Confirmed"
+        dataProvider_contains: "${dataProvider}"
+      }) {
+      id
+      referenceAsset
+      floor
+      inflection
+      cap
+      supplyShort
+      supplyLong
+      expiryTime
+      collateralToken {
         id
-        referenceAsset
-        floor
-        inflection
-        cap
-        supplyInitial
-        supplyShort
-        supplyLong
-        expiryTime
-        collateralToken {
-          id
-          name
-          decimals
-          symbol
-        }
-        collateralBalanceShortInitial
-        collateralBalanceLongInitial
-        collateralBalance
-        shortToken {
-          id
-          name
-          symbol
-          decimals
-          owner
-        }
-        longToken {
-          id
-          name
-          symbol
-          decimals
-          owner
-        }
-        finalReferenceValue
-        statusFinalReferenceValue
-        redemptionAmountLongToken
-        redemptionAmountShortToken
-        statusTimestamp
-        dataProvider
-        redemptionFee
-        settlementFee
-        createdBy
-        createdAt
-        capacity
-        expiryTime
-        challenges {
-          challengedBy
-          proposedFinalReferenceValue
-        }
+        name
+        decimals
+        symbol
+      }
+      collateralBalanceGross
+      gradient
+      collateralBalance
+      shortToken {
+        id
+        name
+        symbol
+        decimals
+        owner
+      }
+      longToken {
+        id
+        name
+        symbol
+        decimals
+        owner
+      }
+      finalReferenceValue
+      statusFinalReferenceValue
+      payoutLong
+      payoutShort
+      statusTimestamp
+      dataProvider
+      protocolFee
+      settlementFee
+      createdBy
+      createdAt
+      submissionPeriod
+      challengePeriod
+      reviewPeriod
+      fallbackSubmissionPeriod
+      permissionedERC721Token
+      capacity
+      expiryTime
+      challenges {
+        challengedBy
+        proposedFinalReferenceValue
       }
     }
-  `;
+  }
+`
 
 export const fetchPoolLists = async (page: number, pageSize: number, createdBy: string, graphUrl: string) => {
-    const result = await request(
-        graphUrl,
-        queryPools(
-            (Math.max(page - 1, 0) * pageSize) / 2, // Because there are 2 rows (long and short) in the table for every pool
-            pageSize / 2, // Because there are 2 rows (long and short) in the table for every pool
-            createdBy,
-            undefined,
-        ),
-    );
+	const result = await request(
+		graphUrl,
+		queryPools(
+			(Math.max(page - 1, 0) * pageSize) / 2, // Because there are 2 rows (long and short) in the table for every pool
+			pageSize / 2, // Because there are 2 rows (long and short) in the table for every pool
+			createdBy,
+			undefined,
+		),
+	);
 
-    return result.pools;
+	return result.pools;
 };
