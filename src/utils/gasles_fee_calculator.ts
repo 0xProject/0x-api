1be68015a2520e52dd0d29efa54a221506c009d1
Vic Huang <vic@0x.org>
2023-01-15 18:31:00 -0800

    chore: Add total charged fee amount

--- src/utils/gasles_fee_calculator.ts
+++ src/utils/gasles_fee_calculator.ts
@@ -1,4 +1,5 @@
 import { BigNumber } from '@0x/utils';
+import { ZERO } from '../constants';
 import {
     GasFee,
     GasFeeConfig,
@@ -19,7 +20,7 @@ import {
 const TRANSFER_FROM_GAS = new BigNumber(72e3);
 
 /**
- * Calculate gasless fee.
+ * Calculate gasless fees object which contains total fee amount and a breakdown of integrator, 0x and gas fees.
  *
  * @param feeConfigs Gasless fee configs parsed from input.
  * @param opts sellToken: Address of the sell token.
@@ -27,33 +28,33 @@ const TRANSFER_FROM_GAS = new BigNumber(72e3);
  *             sellTokenAmountPerBaseUnitNativeToken: Amount of sell token per base unit native token.
  *             gasPrice: Estimated gas price.
  *             quoteGasEstimate: The gas estimate to fill the quote.
- * @returns Gasless fees to charge.
+ * @returns Gasless fees object and the total fee amount.
  */
-function calculateGaslessFee(
-    feeConfigs: GaslessFeeConfigs,
+export function calculateGaslessFees(
     opts: {
+        feeConfigs: GaslessFeeConfigs,
         sellToken: string;
         sellTokenAmount: BigNumber;
         sellTokenAmountPerBaseUnitNativeToken: BigNumber;
         gasPrice: BigNumber;
         quoteGasEstimate: BigNumber;
     },
-): GaslessFees {
+): { fees: GaslessFees; totalChargedFeeAmount: BigNumber } {
     const integratorFee = _calculateIntegratorFee({
-        integratorFeeConfig: feeConfigs.integrator,
+        integratorFeeConfig: opts.feeConfigs.integrator,
         sellToken: opts.sellToken,
         sellTokenAmount: opts.sellTokenAmount,
     });
 
     const zeroexFee = _calculateZeroexFee({
-        zeroexFeeConfig: feeConfigs.zeroex,
+        zeroexFeeConfig: opts.feeConfigs.zeroex,
         sellToken: opts.sellToken,
         sellTokenAmount: opts.sellTokenAmount,
         integratorFee,
     });
 
     const gasFee = _calculateGasFee({
-        gasFeeConfig: feeConfigs.gas,
+        gasFeeConfig: opts.feeConfigs.gas,
         sellToken: opts.sellToken,
         sellTokenAmountPerBaseUnitNativeToken: opts.sellTokenAmountPerBaseUnitNativeToken,
         gasPrice: opts.gasPrice,
@@ -62,11 +63,38 @@ function calculateGaslessFee(
         zeroexFee,
     });
 
-    return {
+    const fees = {
         integrator: integratorFee,
         zeroex: zeroexFee,
-        gas: gasFee,
+        gas: gasFee
     };
+
+    return {
+        fees,
+        totalChargedFeeAmount: _calculateTotalChargedFeeAmount(fees),
+    };
+}
+
+function _calculateTotalChargedFeeAmount(fees: GaslessFees): BigNumber {
+    const totalFeeAmount = ZERO;
+
+    // Integrator fee
+    if (fees.integrator && fees.integrator.feeRecipient) {
+        totalFeeAmount.plus(fees.integrator.feeAmount);
+    }
+    // 0x fee
+    if (fees.zeroex && fees.zeroex.feeRecipient) {
+        // If the fee kind is integrator_share, the 0x amount has already been included in integrator amount
+        if (fees.zeroex.kind !== 'integrator_share') {
+            totalFeeAmount.plus(fees.zeroex.feeAmount);
+        }
+    }
+    // Gas fee
+    if (fees.gas && fees.gas.feeRecipient) {
+        totalFeeAmount.plus(fees.gas.feeAmount);
+    }
+
+    return totalFeeAmount;
 }
 
 function _calculateIntegratorFee(opts: {
@@ -151,7 +179,7 @@ function _calculateGasFee(opts: {
     if (opts.zeroexFee && opts.zeroexFee.feeRecipient && opts.zeroexFee.feeAmount.gt(0)) {
         feeRecipients.add(opts.zeroexFee.feeRecipient);
     }
-    if (opts.gasFeeConfig.feeRecipient) {
+    if (opts.gasFeeConfig && opts.gasFeeConfig.feeRecipient) {
         feeRecipients.add(opts.gasFeeConfig.feeRecipient);
     }
 
