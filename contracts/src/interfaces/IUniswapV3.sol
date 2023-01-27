7fadcd4e4764a24178ce698445c26ea072804d0a
Savarn Dontamsetti <sav.dontamsetti@gmail.com>
2023-01-26 20:21:32 -0500

    updating documentation

--- contracts/src/interfaces/IUniswapV3.sol
+++ contracts/src/interfaces/IUniswapV3.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.6;
 
 interface IUniswapV3QuoterV2 {
+    /// @return Returns the address of the Uniswap V3 factory
     function factory() external view returns (IUniswapV3Factory);
 
     // @notice Returns the amount out received for a given exact input swap without executing the swap
@@ -43,12 +44,31 @@ interface IUniswapV3QuoterV2 {
 }
 
 interface IUniswapV3Pool {
+    /// @notice The first of the two tokens of the pool, sorted by address
+    /// @return The token contract address
     function token0() external view returns (address);
 
+    /// @notice The second of the two tokens of the pool, sorted by address
+    /// @return The token contract address
     function token1() external view returns (address);
 
+    /// @notice The pool's fee in hundredths of a bip, i.e. 1e-6
+    /// @return The fee
     function fee() external view returns (uint24);
 
+    /// @notice The 0th storage slot in the pool stores many values, and is exposed as a single method to save gas
+    /// when accessed externally.
+    /// @return sqrtPriceX96 The current price of the pool as a sqrt(token1/token0) Q64.96 value
+    /// tick The current tick of the pool, i.e. according to the last tick transition that was run.
+    /// This value may not always be equal to SqrtTickMath.getTickAtSqrtRatio(sqrtPriceX96) if the price is on a tick
+    /// boundary.
+    /// observationIndex The index of the last oracle observation that was written,
+    /// observationCardinality The current maximum number of observations stored in the pool,
+    /// observationCardinalityNext The next maximum number of observations, to be updated when the observation.
+    /// feeProtocol The protocol fee for both tokens of the pool.
+    /// Encoded as two 4 bit values, where the protocol fee of token1 is shifted 4 bits and the protocol fee of token0
+    /// is the lower 4 bits. Used as the denominator of a fraction of the swap fee, e.g. 4 means 1/4th of the swap fee.
+    /// unlocked Whether the pool is currently locked to reentrancy
     function slot0()
         external
         view
@@ -108,7 +128,12 @@ interface IUniswapV3Pool {
 }
 
 interface IUniswapV3Factory {
-    /// TODO: add some documentation here
+    /// @notice Returns the pool address for a given pair of tokens and a fee, or address 0 if it does not exist
+    /// @dev tokenA and tokenB may be passed in either token0/token1 or token1/token0 order
+    /// @param a The contract address of either token0 or token1
+    /// @param b The contract address of the other token
+    /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
+    /// @return pool The pool address
     function getPool(address a, address b, uint24 fee) external view returns (IUniswapV3Pool pool);
 }
 
