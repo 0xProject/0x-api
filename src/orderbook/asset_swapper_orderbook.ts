3ae17f6e661b1de69ff946d0eb4e23c5a6f9273e
Kyu <kyuhyun217@gmail.com>
2023-02-16 09:32:51 -0800

    Remove unused getBatchOrdersAsync

--- src/orderbook/asset_swapper_orderbook.ts
+++ src/orderbook/asset_swapper_orderbook.ts
@@ -49,22 +49,6 @@ export class AssetSwapperOrderbook extends Orderbook {
         return result;
     }
 
-    public async getBatchOrdersAsync(
-        makerTokens: string[],
-        takerToken: string,
-        pruneFn?: (o: SignedLimitOrder) => boolean,
-    ): Promise<SignedLimitOrder[][]> {
-        const apiOrders = await this.orderbookService.getBatchOrdersAsync(DEFAULT_PAGE, DEFAULT_PER_PAGE, makerTokens, [
-            takerToken,
-        ]);
-        const orders = apiOrders.records.map(apiOrderToOrderbookOrder);
-        const pruned = pruneFn ? orders.filter(pruneFn) : orders;
-        const groupedByMakerToken: SignedLimitOrder[][] = makerTokens.map((token) =>
-            pruned.filter((o) => o.order.makerToken === token),
-        );
-        return groupedByMakerToken;
-    }
-
     public async destroyAsync(): Promise<void> {
         return;
     }
